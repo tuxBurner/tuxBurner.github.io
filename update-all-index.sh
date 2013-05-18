108fe655d362ea9ba972be8be12f59a6ba14374d
tuxburner <sebasth@gmx.de>
2013-05-18 17:21:56 +0200

    Release off version 1.4.3 for play-jsmessages

---
+++ update-all-index.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+for i in $(find repo -type d); do
+   (cd $i; pwd; ~/workspace_play/tuxburner.github.io/update-index.sh > index.html)
+done
+
+for i in $(find repo-snapshots -type d); do
+   (cd $i; pwd; ~/workspace_play/tuxburner.github.io/update-index.sh > index.html)
+done
