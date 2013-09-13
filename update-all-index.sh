235512df13e2f66784d4762c834ef6ce8f23890d
Sebastian Hardt <s.hardt@micromata.de>
2013-09-13 09:35:50 +0200

    removed snapshots and added new jsroute plugin

--- update-all-index.sh
+++ update-all-index.sh
@@ -4,6 +4,6 @@ for i in $(find repo -type d); do
    (cd $i; pwd; ~/workspace_play/tuxBurner.github.io/update-index.sh > index.html)
 done
 
-for i in $(find repo-snapshots -type d); do
-   (cd $i; pwd; ~/workspace_play/tuxBurner.github.io/update-index.sh > index.html)
-done
+#for i in $(find repo-snapshots -type d); do
+#   (cd $i; pwd; ~/workspace_play/tuxBurner.github.io/update-index.sh > index.html)
+#done
